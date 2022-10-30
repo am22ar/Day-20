6b1225391c5a082e676e4c1b1a27dcc8efc8b9db
am22ar <amarpatil12222@gmail.com>
2022-10-30 15:49:19 +0530

    [Addded: validation of last name]

--- Day20/src/com/JUnit/UserRegistrationTest.java
+++ Day20/src/com/JUnit/UserRegistrationTest.java
@@ -1,17 +1,33 @@
 package com.JUnit;
+
 import org.junit.Assert;
 import org.junit.Test;
+
 public class UserRegistrationTest {
 	userRegistration userRegistration = new userRegistration();
-	 @Test
-	    public void givenFirstName_WhenProper_ShouldReturnTrue() {
-	        boolean result = userRegistration.firstName("Sangeeta");
-	        Assert.assertEquals(true, result);
-	    }
 
-	    @Test
-	    public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
-	        boolean result = userRegistration.firstName("sangeeta");
-	        Assert.assertEquals(false, result);
-	    }
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Amar");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.firstName("amar");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Math");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("math");
+		Assert.assertEquals(false, result);
+
+	}
 }
