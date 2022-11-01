34d6e63cf247f78c7a5a88bf4bee83b2039d932a
am22ar <94344698+am22ar@users.noreply.github.com>
2022-11-01 07:58:19 +0530

    Update UserRegistrationTest.java

--- Day20/src/com/JUnit/UserRegistrationTest.java
+++ Day20/src/com/JUnit/UserRegistrationTest.java
@@ -5,13 +5,13 @@ public class UserRegistrationTest {
 	userRegistration userRegistration = new userRegistration();
 	 @Test
 	    public void givenFirstName_WhenProper_ShouldReturnTrue() {
-	        boolean result = userRegistration.firstName("Sangeeta");
+	        boolean result = userRegistration.firstName("Amar");
 	        Assert.assertEquals(true, result);
 	    }
 
 	    @Test
 	    public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
-	        boolean result = userRegistration.firstName("sangeeta");
+	        boolean result = userRegistration.firstName("amar");
 	        Assert.assertEquals(false, result);
 	    }
 }
